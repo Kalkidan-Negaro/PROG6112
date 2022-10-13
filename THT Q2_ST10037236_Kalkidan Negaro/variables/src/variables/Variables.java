1c34218f1d41473877dd9779aaa763ea0daefe95
Kalkidan-Negaro <104414454+Kalkidan-Negaro@users.noreply.github.com>
2022-10-13 18:43:49 +0200

    Add files via upload

---
+++ THT Q2_ST10037236_Kalkidan Negaro/variables/src/variables/Variables.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//no typing here
+package variables;
+
+/**
+ *
+ * @author DISD
+ // no typing here
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //this is where we type
+    String firstName = "Joe Blog"; //declared a string variable to hold a persons firstname
+    int age = 10; // declared a numerical value to hold a persons age; initialized to hold a value of 10
+   double weight = 55.55;
+   
+   
+   System.out.println(" Personal details"); // literal string; orint out as is
+   System.out.println("-------------------"); // literal string; prin out as is
+   System.out.println("Name: " + firstName);// calls firstName
+   System.out.println("Age: " + age);// calls age
+   System.out.println("Weight: " + weight);// calls weight
+    }
+    
+}
